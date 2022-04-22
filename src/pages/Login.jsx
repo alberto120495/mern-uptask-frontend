50ea5b2df578dd2b39252465d0016b45024e47f1
Alberto <albertopimentel04@gmail.com>
2022-04-21 20:55:42 -0500

    Funcionamiento del Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/clienteAxios";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ([email, password].includes("")) {
+      setAlerta({
+        msg: "Todos los campos son obligatorios",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const { data } = await clienteAxios.post("/usuarios/login", {
+        email,
+        password,
+      });
+      setAlerta({});
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+
+  const { msg } = alerta;
   return (
     <>
       <h1 className="text-sky-600 font-medium text-5xl capitalize">
         Inicia sesion y administra tus{" "}
         <span className="text-slate-700">proyectos</span>
       </h1>
-
-      <form className="my-10 bg-white shadow-md rounded-md p-5">
+      {msg && <Alerta alerta={alerta} />}
+      <form
+        className="my-10 bg-white shadow-md rounded-md p-5"
+        onSubmit={handleSubmit}
+      >
         <div className="my-2">
           <label
             htmlFor="email"
@@ -22,6 +58,8 @@ function Login() {
             name="email"
             placeholder="Ingresa tu email"
             className="w-full px-3 py-2 text-gray-700 border rounded-xl outline-none focus:border-sky-600 focus:shadow-outline"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -38,6 +76,8 @@ function Login() {
             name="password"
             placeholder="Ingresa tu contraseña"
             className="w-full px-3 py-2 text-gray-700 border rounded-xl outline-none focus:border-sky-600 focus:shadow-outline"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <input
